49e4428f3b3d772acaff6fdbb31d3c09601c1f10
LenoxLow <lenox_low.93@hotmail.com>
2016-12-04 18:35:48 +0800

    Sunday Work

---
+++ Data Preprocessing/LabelDriver.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+from pandas import set_option
+set_option("display.max.rows", 20)
+
+# Data pre-set
+name = "KMH"
+be1 = "Good"
+be2 = "Good"
+be3 = "Bad"
+
+# read from vehicle telemetric data set
+pathname1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path1.csv"
+vtele = pd.read_csv(pathname1, sep='\t')
+
+vtele['Behaviour'] = be1
+
+output1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path1.csv"
+vtele.to_csv(output1, sep='\t', encoding='utf-8')
+
+# Path 2 Add on Driver Name
+pathname2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path2.csv"
+vtele2 = pd.read_csv(pathname2, sep='\t')
+
+vtele2['Behaviour'] = be2
+
+output2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path2.csv"
+vtele2.to_csv(output2, sep='\t', encoding='utf-8')
+print(vtele2.tail(1))
+
+
+# Path 3 Add on Driver Name
+pathname3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path3.csv"
+vtele3 = pd.read_csv(pathname3, sep='\t')
+
+vtele3['Behaviour'] = be3
+
+output3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path3.csv"
+vtele3.to_csv(output3, sep='\t', encoding='utf-8')
+print(vtele3.tail(1))
--- Data Preprocessing/addDriverName.py
+++ Data Preprocessing/addDriverName.py
@@ -6,12 +6,21 @@ from pandas import set_option
 set_option("display.max.rows", 20)
 
 # Data pre-set
-name = "TWT"
-fullname = "Tan Wee Ther"
+name = "OYC"
+h1 = 0
+h3 = 5
+fullname = "Ooi Yong Cheng"
 
 # read from vehicle telemetric data set
 pathname1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step I/" + name + "Path1.csv"
 vtele = pd.read_csv(pathname1, sep='\t')
+
+
+# delete the first h rows from data set
+print(len(vtele.index))
+for j in range(0, h1):
+   vtele.drop(vtele.head(1).index, axis=0, inplace=True)
+   vtele.reset_index(drop=True)
 
 vtele['Driver Name'] = fullname
 
@@ -33,6 +42,11 @@ print(vtele2.tail(1))
 # Path 3 Add on Driver Name
 pathname3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step I/" + name + "Path3.csv"
 vtele3 = pd.read_csv(pathname3, sep='\t')
+
+print(len(vtele3.index))
+for j in range(0, h3):
+   vtele3.drop(vtele3.head(1).index, axis=0, inplace=True)
+   vtele3.reset_index(drop=True)
 
 vtele3['Driver Name'] = fullname
 
