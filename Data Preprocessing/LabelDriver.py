9bc99fa953beec49dd2901314c28387b75ab9835
LenoxLow <lenox_low.93@hotmail.com>
2017-01-19 10:04:49 +0800

    Added Passenger evaluation behaviour data

--- Data Preprocessing/LabelDriver.py
+++ Data Preprocessing/LabelDriver.py
@@ -8,33 +8,39 @@ name = "GCW"
 be1 = "Good"
 be2 = "Good"
 be3 = "Good"
+h1 = 2
 
 # read from vehicle telemetric data set
-pathname1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path1.csv"
+pathname1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step IV/" + name + "Path1.csv"
 vtele = pd.read_csv(pathname1, sep='\t')
+
+print(len(vtele.index))
+for j in range(0, h1):
+   vtele.drop(vtele.head(1).index, axis=0, inplace=True)
+   vtele.reset_index(drop=True)
 
 vtele['Behaviour'] = be1
 
-output1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path1.csv"
+output1 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step V/" + name + "Path1.csv"
 vtele.to_csv(output1, sep='\t', encoding='utf-8')
 
 # Path 2 Add on Driver Name
-pathname2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path2.csv"
+pathname2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step IV/" + name + "Path2.csv"
 vtele2 = pd.read_csv(pathname2, sep='\t')
 
 vtele2['Behaviour'] = be2
 
-output2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path2.csv"
+output2 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step V/" + name + "Path2.csv"
 vtele2.to_csv(output2, sep='\t', encoding='utf-8')
 print(vtele2.tail(1))
 
 
 # Path 3 Add on Driver Name
-pathname3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step II/" + name + "Path3.csv"
+pathname3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step IV/" + name + "Path3.csv"
 vtele3 = pd.read_csv(pathname3, sep='\t')
 
 vtele3['Behaviour'] = be3
 
-output3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step III/" + name + "Path3.csv"
+output3 = "/Users/jiaminglow/Desktop/FYP Part II/Process Step V/" + name + "Path3.csv"
 vtele3.to_csv(output3, sep='\t', encoding='utf-8')
 print(vtele3.tail(1))
